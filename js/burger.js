a4107126131b0904757e88c3fdb45f0d0d4c0558
Kindeme <fkounoudji@gmail.com>
2020-02-02 21:38:20 -0600

    contact page

--- js/burger.js
+++ js/burger.js
@@ -3,17 +3,16 @@
 (function(){
   // fixed navbar
   const fixedNav = document.querySelector('.navbar');
+  const navBtn =document.querySelector('.nav-button');
+  const navLinks =document.querySelector('.navlinks');
 
 window.addEventListener('scroll',function(){
 
-  if(window.pageYOffset> 84){
+  if(window.pageYOffset> 70){
     fixedNav.classList.add('fixed');
-
   }else{
     fixedNav.classList.remove('fixed');
   }
 });
  
-
-
 })();
