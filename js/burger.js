8315eec787b356a787c9fc5e3ffb212c55f229b9
Kindeme <fkounoudji@gmail.com>
2020-02-02 22:15:15 -0600

    contact page

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,9 @@
 // NAvigation function
 
 (function(){
-  // fixed navbar
+  
   const fixedNav = document.querySelector('.navbar');
-  const navBtn =document.querySelector('.nav-button');
-  const navLinks =document.querySelector('.navlinks');
-
+// fixed navbar
 window.addEventListener('scroll',function(){
 
   if(window.pageYOffset> 70){
@@ -14,5 +12,29 @@ window.addEventListener('scroll',function(){
     fixedNav.classList.remove('fixed');
   }
 });
- 
 })();
+
+// responsible navbar
+(function(){
+  const navBtn = document.querySelector('.nav-button');
+  const navLinks = document.querySelector('.navlinks');
+  navBtn.addEventListener('click',function(){  
+    let value = navLinks.classList.contains('showNav');
+    if(value){
+      navLinks.classList.remove('showNav');
+      navBtn.classList.remove('change');
+    }
+    else{
+      navLinks.classList.add('showNav');
+      navBtn.classList.add('change');
+    }
+  
+  });
+
+})();
+
+//copyright date
+const date = document.querySelector('#date');
+ date.innerHTML = new Date().getFullYear();
+
+
