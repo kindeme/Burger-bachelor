67f2afd7522030562c28913f79f266bce622ca4f
Kindeme <fkounoudji@gmail.com>
2020-02-04 10:23:30 -0600

    js

--- js/burger.js
+++ js/burger.js
@@ -40,25 +40,85 @@ window.addEventListener('scroll',function(){
     const btnRight = document.querySelector('.btn-right');
 
     let counter = 0;
-
           btnLeft.addEventListener("click", function(){
             counter--;
             if (counter<1){
               counter = bcg.length;
             }
-            
-            headerBcg.style.backgroundImage = `url('../images/headerbcg-${counter}.jpg')`;
+                 headerBcg.style.backgroundImage = `url('../images/headerbcg-${counter}.jpg')`;
            
               });
 
               btnRight.addEventListener("click", function(){
-                if (counter>bcg.length-1){
-                  counter = 0;
-                }
                 counter++;
-                headerBcg.style.backgroundImage = `url('../images/headerbcg-${counter}.jpg')`;
+                if (counter>bcg.length){
+                  counter = 1;
+                }
                
+                headerBcg.style.backgroundImage = `url('../images/headerbcg-${counter}.jpg')`;
                   });
+
+// testimonial js
+
+//testimonial setup
+let allTestimonial = [{
+  text: ` " Honest conflict has more social value than dishonest harmony. "`,
+  name: `Joe Rogan`,
+  image: `url('../images/customer-0.jpg')`
+},
+{
+  text: ` " If you begin to understand what you are without trying to change it, then what you are undergoes a transformation.. "`,
+name: `Jiddu Krishnamurti`,
+image: `url('../images/customer-1.jpg')`
+},
+{
+text: ` " happen. Make it happen. Make your own future. Make your own hope. Make your own love. And whatever your beliefs, honor your creator, not by passively waiting for grace to come down from upon high, but by doing what you can to make grace happen... yourself, right now, right down here on Earth.. "`,
+name: `Bradley Whitford`,
+image: `url('../images/customer-2.jpg')`
+},
+{
+  text: ` "The greatest discovery of all time is that a person can change his future by merely changing his attitude. "`,
+  name: `Oprah Winfrey`,
+  image: `url('../images/customer-3.jpg')`
+  },
+  {
+  text: ` "Decide what you want, decide what you are willing to exchange for it. Establish your priorities and go to work."`,
+  name: `H. L. Hunt`,
+  image:`url('../images/customer-4.jpg')`
+  }
+];
+const customerText = document.querySelector('.customer-text');
+const customerImage = document.querySelector('#customer-img');
+console.log(customerImage);
+const customerName = document.querySelector('.customer-name');
+const prevBtn = document.querySelector('.prevBtnt');
+const nexBtn = document.querySelector('.nexBtnt');
+
+let testiCount = 0;
+prevBtn.addEventListener("click",function(){
+  testiCount --;
+  if(testiCount<0){
+    testiCount = allTestimonial.length-1;
+  }
+  customerText.textContent = allTestimonial[testiCount].text;
+  customerImage.src = allTestimonial[testiCount].image;
+  customerName.textContent = allTestimonial[testiCount].name;
+
+
+});
+
+nexBtn.addEventListener("click",function(){
+  testiCount ++;
+  if(testiCount> allTestimonial.length-1){
+    testiCount = 0;
+  }
+  customerText.textContent = allTestimonial[testiCount].text;
+  customerName.textContent = allTestimonial[testiCount].name;
+  customerImage.scr = allTestimonial[testiCount].image;
+
+
+});
+
 })();
 
 
